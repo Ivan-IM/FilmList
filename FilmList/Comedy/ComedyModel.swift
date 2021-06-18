3100005ab0719f4edbe5953dd97a28a92e4eb84e
ilabrosimov <ilabrosimov@gmail.com>
2021-06-18 10:54:09 +0300

    Добавлена моделька. Добавлена связь между ComedyTableViewController и ComedyModel

--- FilmList/Comedy/ComedyModel.swift
+++ FilmList/Comedy/ComedyModel.swift
@@ -5,4 +5,25 @@
 //  Created by Иван Маришин on 18.06.2021.
 //
 
-import Foundation
+struct Comedy {
+    let title: String
+    let subTitle: String
+    
+    static func getComedies () -> [Comedy] {
+    
+        let comedies: [Comedy] = [
+            Comedy(title: "Криминальное чтиво", subTitle: "Pulp fiction, 1994"),
+            Comedy(title: "1+1", subTitle: "Intouchables, 2011"),
+            Comedy(title: "Операция Ы и другие приключения Шурика", subTitle: "1965"),
+            Comedy(title: "Один дома", subTitle: "Home alone, 1990"),
+            Comedy(title: "12 стульев", subTitle: "1971"),
+            Comedy(title: "Невероятная история Уолтера Митти", subTitle: "Secret life of Walter Mitty, 2013"),
+            Comedy(title: "Стажер", subTitle: "Intern, 2015"),
+            Comedy(title: "Мальчишник в Вегасе", subTitle: "Hangover, 2009")
+        ]
+        
+        return comedies
+    }
+    
+}
+
