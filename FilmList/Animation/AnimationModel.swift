48072505c6d086326452b916f4fdfbf56758a6e3
Yuri Martinovsky <vvrite@me.com>
2021-06-19 20:08:24 +0300

    Вывод фильмов на вкладку Animation

--- FilmList/Animation/AnimationModel.swift
+++ FilmList/Animation/AnimationModel.swift
@@ -8,4 +8,16 @@
 struct Animation {
     let title: String
     let description: String
+    let image: String
+}
+
+extension Animation {
+    static func getAnimation() -> [Animation] {
+        [Animation(title: "Корпорация монстров", description: "США, 28 октября 2001", image: "Monsters Inc"),
+         Animation(title: "Тачки", description: "США, 14 марта 2006", image: "Cars"),
+         Animation(title: "ВАЛЛИ", description: "США, 21 июня 2008", image: "walle"),
+         Animation(title: "В поисках Немо", description: "США, 18 мая 2003", image: "Finding Nemo"),
+         Animation(title: "Ледниковый период", description: "США, 12 марта 2002", image: "Ice Age")
+        ]
+    }
 }
