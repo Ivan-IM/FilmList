f52760c6010ec568b179a75771479b5ea1664335
iVlady <vinogrotskiy97@gmail.com>
2021-06-19 20:59:58 +0300

    Managed colors

--- FilmList/Horror/HorrorTableViewController.swift
+++ FilmList/Horror/HorrorTableViewController.swift
@@ -14,13 +14,8 @@ class HorrorTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+        tableView.backgroundColor = .systemTeal
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-
-        
     }
 
     // MARK: - Table view data source
@@ -53,7 +48,10 @@ class HorrorTableViewController: UITableViewController {
         cell.textLabel?.text = singleHorror.name
         cell.detailTextLabel?.text = singleHorror.author
         cell.imageView?.image = UIImage(named: singleHorror.image)
+        
         cell.selectionStyle = .none
+        cell.backgroundColor = .systemTeal
+        
         
 
         return cell
